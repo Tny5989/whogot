73c5e36d0e1a2a53ca2d097dd0d7756a2ae198a4
= <=>
2024-01-12 09:40:00 -0600

    readd name logic... for now.

--- whogot.lua
+++ whogot.lua
@@ -30,7 +30,7 @@ TODO:   Make sure the mobType filter is correct
 
 addon.author = 'InoUno & Tny5989'
 addon.name = 'WhoGot'
-addon.version = '1.0.0.4'
+addon.version = '1.0.0.3'
 
 require('common')
 local settings = require('settings');
@@ -141,11 +141,12 @@ ashita.events.register('packet_in', 'packet_in_cb', function(e)
     end
 
     local updateMask = struct.unpack('B', e.data_modified, 0x0A + 1)
-    local hasClaimId = (bit.band(updateMask, 2) > 0)
+    local hasClaimInfo = (bit.band(updateMask, 2) > 0)
+    local hasClaimName = (hasClaimInfo and (bit.band(updateMask, 8) == 0))
     local mobDisappear = (whoGot.settings.pruneDead and (bit.band(updateMask, 32) > 0))
     local time = os.date('[%I:%M:%S]', os.time())
 
-    if (not hasClaimId and not mobDisappear) then
+    if (not hasClaimInfo and not mobDisappear) then
         return false
     end
 
@@ -164,6 +165,15 @@ ashita.events.register('packet_in', 'packet_in_cb', function(e)
     if (mobType ~= 2) then
         -- Non-killable NPCs
         return false;
+    end
+
+    if (hasClaimName) then
+        local claimerName = struct.unpack('s', e.data_modified, 0x34 + 1)
+        if (string.len(claimerName) > 0) then
+            print.Debug(string.format('adding claimer(%s) for mob(%d) with mobType(%d) at(%s)', claimerName, mobIndex, mobType, time))
+            whoGot.claims[mobIndex] = { claimer = tostring(claimerName), time = time }
+            return false
+        end
     end
 
     local claimerId = struct.unpack('I', e.data_modified, 0x2C + 1)
